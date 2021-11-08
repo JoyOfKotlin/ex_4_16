19c6520ae0f2db43e72f0e24944968e8376bee8f
Denis Manikhin <dmanikhine@gmail.com>
2021-11-08 08:38:24 +0100

    Final commit

--- app/src/main/kotlin/ex_4_16/App.kt
+++ app/src/main/kotlin/ex_4_16/App.kt
@@ -3,13 +3,24 @@
  */
 package ex_4_16
 
+fun <T> iterate(seed: T, f: (T) -> T, n: Int): List<T> {
+    tailrec fun iterate_(acc: List<T>, seed: T): List<T> =
+            if (acc.size <n)
+                iterate_(acc + seed, f(seed))
+            else
+                acc
+    return iterate_(listOf(), seed)
+}
+
+
 class App {
     val greeting: String
         get() {
-            return "Hello World!"
+            return "package ex_4_16"
         }
 }
 
 fun main() {
     println(App().greeting)
+	println(iterate(1,{it+1},10))
 }
